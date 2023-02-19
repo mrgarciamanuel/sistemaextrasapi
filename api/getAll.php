f8aaf1125e913a82822cc87407b47f837ac6f8c3
Garcia Manuel <garciaisaiasmauel@gmail.com>
2023-02-19 12:59:45 +0000

    novo get all

--- api/getAll.php
+++ api/getAll.php
@@ -1,4 +1,34 @@
 <?php 
-echo "Funciona";
+header("Acess-control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+//initialization of all services
+include('../models/initialize.php');
+
+//instatiation of employee
+$employee = new Employee($db);
+$result = $employee->get_all();
+
+if($result->rowCount() > 0){
+    $employee_arr = array();
+    $employee_arr['data'] = array();
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+
+        $employee_item = array(
+            'id' => $id,
+            'name' => $name,
+            'surname' => $surname,
+            'username' => $username,
+            'gender' => $gender,
+            'description' => $description,
+            'tsdia' => $tsdia         
+        );
+        array_push($employee_arr['data'], $employee_item);
+    }
+    echo json_encode($employee_arr);
+}else{
+    echo json_encode(array('mensage'=>'Non information was found'));
+}
 
 ?>
